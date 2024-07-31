fe4803b410b7c67b8c1d5f7270246edb056fd3e3
Chris-Mark-Wilson <chris@cmwilson.co.uk>
2024-07-31 09:22:38 +0100

    updated glossary

--- src/pages/Glossary.jsx
+++ src/pages/Glossary.jsx
@@ -1,21 +1,18 @@
-
+import {PictureCard} from "../components/PictureCard";
 export const Glossary = () => {
   return (
     <>
       
       <h1>Glossary</h1>
       <section className="picture-card" id="sips">
-        {/* <h4>Sips</h4>
-        <p>
-          'Sips' stands for Structural Insulated Panels. They are built with a modern construction method that uses a composite of two materials, typically a foam core sandwiched between two sheets of oriented strand board (OSB). They are used to build ultra modern, energy efficient houses, well suited to air source or ground source heat pumps and other renewable energy sources.
-        </p>
-        <img
-          ref={ref1}
-          className={`card-picture fade-in ${isVisible1 ? "visible" : ""}`}
-          src="./pictures/cement-clad-during.jpg"
-          alt="sipd house under construction"
-          title="An ultra modern 'Sips' house"
-        /> */}
+        <h4>Sips</h4>
+        <PictureCard
+        imageUris={{0:'./pictures/cement-clad-during.jpg'}}
+        alts={{0:'sips house under construction'}}
+        paragraphs={{1:'Sips stands for Structural Insulated Panels. They are built with a modern construction method that uses a composite of two materials, typically a foam core sandwiched between two sheets of oriented strand board (OSB). They are used to build ultra modern, energy efficient houses, well suited to air source or ground source heat pumps and other renewable energy sources.'}}
+        />
+        
+      
       </section>
     </>
   );
