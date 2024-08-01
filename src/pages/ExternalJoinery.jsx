5ca8464a0dfda492251ce4b9e15cb67541392e54
chris <chris@cmwilson.co.uk>
2024-08-01 15:20:25 +0100

    add external joinery content

--- src/pages/ExternalJoinery.jsx
+++ src/pages/ExternalJoinery.jsx
@@ -5,16 +5,29 @@ export const ExternalJoinery = () => {
     return (
         <div className='page'>
         <h1>External Joinery</h1>
-        <section className='description'>A selection a various external buildings I've completed over recent years.</section>
+        <section className='description'>A selection a various external buildings and other external woodwork I've completed over recent years.</section>
         <br/>
         <hr/>
+        <p>A heavy duty bespoke external staircase to be used as a fire excape</p>
+        <PictureCard
+        imageUris={{
+            0:'./pictures/bespoke-stairs-workshop3.jpg',
+        
+            2:'./pictures/bespoke-stairs-workshop.jpg',
+            3:'./pictures/bespoke-stairs-cox.jpg',}}
+        alts={{}}
+        paragraphs={{}}
+        />
+        <br/>
+        <hr/>
+        {/*--------------------------------------------------------------------------------*/}
         <Accordion >
             <Accordion.Item eventKey={0}>
                 <Accordion.Header bsPrefix={'accordian-headers'}>Case study - Garden Summer House - Mrs Parr Castor, Peterborough</Accordion.Header>
                 <Accordion.Body>
 
-<section className='case-study'>
-    <p>A summerhouse build with external decking</p>
+        <section className='case-study'>
+        <p>A summerhouse build with external decking - lockdown project</p>
         <PictureCard
         imageUris={{
             0:'./pictures/louisa-summerhouse-floor-joists.jpg',
@@ -25,7 +38,7 @@ export const ExternalJoinery = () => {
         paragraphs={{1:'Posts were dug in and the ground beneath covered with heavy duty weed control fabric. 6x2 treated timbers were used for the joists, keeping the entire structure off the ground to allow for ventilation underneath. This way, nothing rots away from being in contact with the wet ground.'}}
         />
 
-<PictureCard
+        <PictureCard
         imageUris={{1:'dist/pictures/louisa-summerhouse-posts.jpg'
   
         }}
@@ -33,13 +46,13 @@ export const ExternalJoinery = () => {
         paragraphs={{0:'The decking was now installed, this gave me a working platform for the rest of the build'}}
         />
 
-<PictureCard
+        <PictureCard
         imageUris={{0:'./pictures/louisa-summerhouse-framework.jpg',}}
         alts={{}}
         paragraphs={{1:'The timber framework was made up from simple 3x2 cls timbers, the real strength added later by covering the internal walls with 18mm waterproof ply. This ensured the structure was kept stiff and square. The internal floor was a sandwich of damp proof membrane, 18mm ply, 25mm cellotex insulation and 18mm tongue and grooved chipboard on top. This was all glued and screwed together to form a solid base'}}
         />
 
-<PictureCard
+        <PictureCard
         imageUris={{1:'./pictures/louisa-summerhouse-felted.jpeg'}}
         alts={{}}
         paragraphs={{0:'60mm cellotex insulation was fitted between the wall studs and the rafters, then the entire structure was covered with a breathable waterproof membrane. Second hand uPVC windows and doors were sourced, cleaned and fitted with new hardware'}}
@@ -68,6 +81,66 @@ export const ExternalJoinery = () => {
         </Accordion.Body>
         </Accordion.Item>
         </Accordion>
+        
+        <hr/>
+        {/*--------------------------------------------------------------------------------*/}
+        <p>Lockdown - social distancing</p>
+        <PictureCard
+        imageUris={{
+            0:'./pictures/dog-doublet-pizza-roof.jpg',
+        }}
+        alts={{}}
+        paragraphs={{1:'A canopy at the Dog in a Doublet pub in Peterborough for use as an external pizza parlour. A lockdown project dictated by social distancing rules at the time. Simple heavy duty framework and an eternit slate roof. We worked as a three man team under the landlord Johns direction, and went on to complete several other interesting projects for him during this period'}}
+        />
+        <br/>
+       <p> The next job was an outside dining area seating up to 6 people</p>
+        <PictureCard
+        imageUris={{
+            0:'./pictures/dog-doublet-china-hat-poles.jpg',
+            1:'./pictures/dog-doublet-china-hat-floor-joists.jpg',
+            2:'./pictures/dog-doublet-china-hat-skeleton.jpg',
+            3:'./pictures/dog-doublet-china-hat-during2.jpg'
+        }}
+        alts={{}}
+        paragraphs={{}}
+        />
+        <br/>
+        <p>The structure was purchased second hand, remodelled to sit on stilts then re-roofed</p>
+
+        <PictureCard
+        imageUris={{
+            2:'./pictures/dog-doublet-china-hat-during3.jpg',
+            3:'./pictures/dog-doublet-finished-china-hat.jpg',
+            4:'./pictures/dog-doublet-finished-china-hat2.jpg',
+            1:'./pictures/dog-soublet-chaina-hat-during.jpg'
+        }}
+        alts={{}}
+        paragraphs={{}}
+        />
+
+        <hr/>
+        {/*--------------------------------------------------------------------------------*/}
+        
+        <PictureCard
+        imageUris={{
+            0:'./pictures/louisas-french-doors.jpg',
+            2:'./pictures/louisas-big-shed-during.jpg'
+        }}
+        alts={{}}
+        paragraphs={{1:'pair of bespoke french doors made to fit a reclaimed frame, and a bit of a shed roof, mostly to make use of left over or reclaimed materials'}}
+        />
+       
+         <br/>
+         <hr/>
+         <PictureCard
+        imageUris={{1:'./pictures/hot-tub-cox.jpg'
+
+        }}
+        alts={{}}
+        paragraphs={{0:'A hot tub surround, with a removable panel for access to the pump and heater, with a small decked area to the front along with a covered summerhouse/pergola'}}
+        />
+        <br/>
+        <hr/>
         </div>
     );
     }
