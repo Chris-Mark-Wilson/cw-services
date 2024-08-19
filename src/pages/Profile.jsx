c7e141e622f2562268c53e9e557d79907bd5e525
chris <chris@cmwilson.co.uk>
2024-08-19 15:35:39 +0100

    add css to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { UserContext } from '../context/UserContext';
 import { uploadProfilePic } from '../../api/firebaseAuth';
 import "../css_files/profile.css";
 
+
 export const Profile = () => {
 
     const {user} = useContext(UserContext);
@@ -11,7 +12,15 @@ export const Profile = () => {
     const [profileImageURL,setProfileImageURL]=useState('');
     const [newImage,setNewImage]=useState('');
     const [passwordInputType,setPasswordInputType] =useState('password');
+    const [oldPassword,setOldPassword]=useState('');  
+    const [passwordError,setPasswordError]=useState('');
+    const [newPassword,setNewPassword]=useState('');
 
+    
+
+
+    
+    
     useEffect(()=>{
         if(user && user.photoURL){
             setProfileImageURL(user.photoURL);
@@ -30,10 +39,18 @@ const handleUpload= async()=>{
     }catch(error){
         console.log('error uploading profile pic:',error);
     }
-
+    
   
 }
 
+
+const showPassword = () => {
+if (passwordInputType === "password") {
+  setPasswordInputType("text");
+} else {
+  setPasswordInputType("password");
+}
+}
 
         return (
           user && (
@@ -44,15 +61,12 @@ const handleUpload= async()=>{
                 <section className="profile-details">
                   <h5>Profile</h5>
                   <div className="profile-item">
-               
                     <p>Display name:</p>
                     <p>
-                    
                       {user.displayName != null ? user.displayName : "no user"}
                     </p>
                   </div>
                   <div className="profile-item">
-                    
                     <p>Email:</p>
                     <p>
                       {user.email}
@@ -72,41 +86,100 @@ const handleUpload= async()=>{
                     {" "}
                     <p>Logged in since:</p>
                     <p>
-                     
                       {new Date(+user.metadata.lastLoginAt).toLocaleString()}
                     </p>
                   </div>
                   <div className="profile-item">
-                  
                     <p>Status:</p>
                     <p> {user.isAdmin ? "Admin" : "Member"}</p>
                   </div>
                   <div className="profile-item">
-                   
                     <p>Profile picture:</p>
                     <p>
-                     
                       <img src={profileImageURL} alt="profile image" />
                     </p>
                   </div>
                 </section>
 
-                <section className='update-profile'>
-                <h5>Update Profile</h5>
-      
-                    <div className='profile-item'>
-                <p>Upload profile image:</p>
-                <input type='file' accept='image/*' name='updateImage'
-               onChange={(e)=>setNewImage(e.target.files[0])}/></div>
+                <section id="update-profile">
+                  <h5>Update Profile Picture</h5>
 
-                 {newImage && 
-                    <div className='profile-item'>
-                        <img src={URL.createObjectURL(newImage)}/>
-                        <button onClick={handleUpload}>Upload</button>
-                    </div>}
+                  <div id="upload-image" className="profile-item">
+                    <p>Change profile picture:</p>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      name="updateImage"
+                      onChange={(e) => setNewImage(e.target.files[0])}
+                    />
+                  </div>
 
-             
-                    </section>
+           
+                    <div className="profile-item-upload">
+                      {newImage&&<><img src={URL.createObjectURL(newImage)} />
+                      <button onClick={handleUpload}>Upload</button></>}
+                    </div>
+               
+
+                  <div className="change-password">
+                    <div className="sign-in">
+                      <h5>Change Password</h5>
+                      <div className="password-input-group">
+                        <div className="password-input">
+                          <input
+                            name="password"
+                            type={passwordInputType}
+                            value={oldPassword}
+                            placeholder={"  Old Password"}
+                            autoComplete="old-password"
+                            onChange={(e) => setOldPassword(e.target.value)}
+                            required
+                          />
+                          <button
+                            className="password-eye"
+                            onClick={showPassword}
+                          >
+                            <img src="/images/passwordIcon.png" />
+                          </button>
+                        </div>
+
+                        {passwordError && (
+                          <p className="signin-error">{passwordError}</p>
+                        )}
+                      </div>
+
+                      <div className="password-input-group">
+                        <div className="password-input">
+                          <input
+                            name="password"
+                            type={passwordInputType}
+                            value={newPassword}
+                            placeholder={"  New Password"}
+                            autoComplete="new-password"
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            required
+                          />
+                          <button
+                            className="password-eye"
+                            onClick={showPassword}
+                          >
+                            <img src="/images/passwordIcon.png" />
+                          </button>
+                        </div>
+
+                        {passwordError && (
+                          <p className="signin-error">{passwordError}</p>
+                        )}
+
+                        <div className="profile-item">
+                          <button id="change">Change</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+
+                <button id="delete-account">Delete Account</button>
               </div>
             </>
           )
